87861c3ff65d1c1b628cdf5bdb764f98dc059417
Ethan Lee <elee52@stuy.edu>
2025-05-28 11:59:13 -0400

    worked on functions for rightrotate and rightshift

--- src/sha256.py
+++ src/sha256.py
@@ -13,3 +13,21 @@ def preprocessing(text) -> str:
     binary += format(message_length, '064b')
 
     return binary
+
+
+# The next thing to discuss is “rightrotate”. This means we take a binary number, then take its digit on the far right and place it on the far left of the number. Repeat this for the number of times indicated after the word “rightrotate”.
+
+# Now consider “rightshift”. This means removing the digit on the far right and adding a zero to the far left. Repeat this for the number of times indicated.
+
+
+
+
+def rightrotate(text,c) -> str:
+    s = ""
+    for (i in c):
+        s.append(text.charAt(len(text)-i))
+
+    return s+text.substring(len(text)-c)
+
+def rightshift(text,c) -> str:
+    
