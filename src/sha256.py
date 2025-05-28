3627f5378973b1972e9cff9ec946546aa1bc85a7
Daniel Wu <wudaniel424@gmail.com>
2025-05-28 14:49:06 -0400

    message_scheduling

--- src/sha256.py
+++ src/sha256.py
@@ -13,3 +13,20 @@ def preprocessing(text) -> str:
     binary += format(message_length, '064b')
 
     return binary
+
+def singular_32_split(processed_bits): #split a 512 block into 16 32 bits
+    bitArray = []
+    for i in range(0, len(processed_bits), 32):
+        bitArray.append(processed_bits[i:i+32])
+    for i in range(0, 48):
+        bitArray.append('0'*32)
+    return bitArray
+
+def multiple_32_split(bitArray):
+    split_Array=[]
+    for i in range(0,len(bitArray)):
+        split_Array.append(singular_32_split(bitArray[i]))
+    return split_Array
+
+
+
