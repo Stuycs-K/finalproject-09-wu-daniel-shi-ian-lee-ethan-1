26843a456cd11962d5e400d2e19154a4681123db
Ian Shi <ianshi1026@gmail.com>
2025-05-28 14:18:38 -0400

    changes from 5/27 forgot to push

--- src/util.py
+++ src/util.py
@@ -1,3 +1,5 @@
+import typing
+
 def string_to_binary(text:str) -> str:
     binary = ''.join(format(i, '08b') for i in bytearray(text, encoding = 'utf-8'))
     return binary
@@ -11,9 +13,12 @@ def get_next_multiple_of_512(num: int) -> int:
         next_int += 512
     return next_int
 
-def get_512_bit_chunks(binary:str):
+def get_512_bit_chunks(binary:str) -> typing.List[str]:
     if (len(binary) % 512 != 0):
         raise ValueError('size of binary should be a multiple of 512')
-    chunks = []
     total_chunks = len(binary) / 512
+    chunks = [''] * total_chunks
+    for i in range(total_chunks):
+        chunks[i] = binary[(i*512):((i+1)*512)]
+    return chunks
     
