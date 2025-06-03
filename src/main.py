5d0078fbce721924d652ee6bf1e9d304fe641fa7
Ethan Lee <elee52@stuy.edu>
2025-06-03 12:55:44 -0400

    temp test cases

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ from sha256 import sha256
 #unhashed = input("blah blah blah")
 #print(sha256(unhashed))
 
-print(sha256("abc"))
-print(sha256("hello"))
+#assert(sha256("abc"))=='ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad'
+assert(sha256("hello"))=='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
