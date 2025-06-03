74c561e29f3c1d12fe9a2f3d0635d2eb38def27a
Ethan Lee <elee52@stuy.edu>
2025-06-03 12:19:25 -0400

    better main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from sha256 import sha256
 
-unhashed = input("blah blah blah")
-print(sha256(unhashed))
+#unhashed = input("blah blah blah")
+#print(sha256(unhashed))
+
+print(sha256("abc"))
+print(sha256("hello"))
